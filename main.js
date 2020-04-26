804b86c63068060f412c69b0cadbed3c272b83ee
Jean Luis Soto Robles <jeanRollins>
2020-04-26 19:34:15 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,65 @@
+const nodemailer = require('nodemailer')
+const express = require('express')
+const app = express()
+app.use( express.json() )
+
+app.post('/sendMail' , ( req , res ) => {
+    const send = async () => {
+
+        try {
+            let transporter = await nodemailer.createTransport({
+                host: "smtp.ethereal.email",
+                port: 587,
+                secure: false, 
+                auth: {
+                    user: 'raphaelle39@ethereal.email', 
+                    pass: 'zeR8nFXkRZbkHdmm51' 
+                }
+            })
+
+            transporter.sendMail(req.body , ( error , info) => {
+                if( error) {
+                    res.send({
+                        status   : 500    ,
+                        message  : error  , 
+                        valid    : false  ,
+                        info 
+                    })
+                }
+                else {
+                    res.send({
+                        status  : 200      ,
+                        message : req.body ,
+                        valid   : true     ,
+                        info 
+                    })
+                }
+            }) 
+        } 
+        catch (error) {
+            console.log( 'error : ' , error)
+        }
+    }
+
+    return send()
+})
+
+app.get( '/' , ( req , res ) => {
+
+    const send = async () => {
+        try {
+           res.send({
+               status  : 200 ,
+               message : 'Welcome api email service.'
+           })
+        } 
+        catch (error) {
+            console.log( 'error : ' , error)
+        }
+    }
+    return send()
+})
+
+app.listen( 8000, () => {
+   
+})
