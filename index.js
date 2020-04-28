70a803e54a50c179ae288e0656c7dd77e857aaed
Jean Luis Soto Robles <jeanRollins>
2020-04-28 12:09:40 -0400

    First commit

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ app.get( '/' , ( req , res ) => {
     return send()
 })
 
-app.listen( 7000, () => {
+app.listen( process.env.PORT || 7000, () => {
+   console.log('Status : server OK!' )
    
 })
